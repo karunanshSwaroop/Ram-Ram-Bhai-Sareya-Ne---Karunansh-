b84a2e142b12d280702c5670a3d6aa45b1ba408d
Karunansh swaroop <133811038+karunanshSwaroop@users.noreply.github.com>
2024-01-10 17:43:44 +0530

    Add files via upload

---
+++ MERGE SORTED ARRAY.cpp
@@ -0,0 +1,26 @@
+class Solution {         
+public:
+       //Time Complexity: O(n+m)
+       //space complexity: o(1)
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1;    
+        int j=n-1;    
+        int index=m+n-1;   
+ 
+           while(i>=0 && j>=0)    
+           {
+               if(nums1[i]>=nums2[j])   // 3>6   
+               {
+                   nums1[index--]=nums1[i--];
+               }
+               else
+               {
+                   nums1[index--]=nums2[j--];  //nums[5,4,3]= 6,5,3;
+               }
+           }
+           while(j>=0)     
+           {
+               nums1[index--]=nums2[j--];   
+           }
+    }
+};
