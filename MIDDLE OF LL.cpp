56d1cffba6cb5ca39beac10ac48caf5a38283b7a
Karunansh swaroop <133811038+karunanshSwaroop@users.noreply.github.com>
2024-01-19 20:36:49 +0530

    Add files via upload

---
+++ MIDDLE OF LL.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+//time complexity:O(n)
+    int countNode(ListNode* head)
+    {
+        int count = 0;
+        for (ListNode* tmp = head; tmp; tmp = tmp->next) count++;
+        return count;
+    }
+    ListNode* middleNode(ListNode* head) {
+        int amount = countNode(head);
+        int position = amount / 2 + 1;
+        int count = 0;
+        for (ListNode* tmp = head; tmp; tmp = tmp->next) 
+        {
+            count++;
+            if (count == position) return tmp;
+        }
+        return head;
+    }
+};
